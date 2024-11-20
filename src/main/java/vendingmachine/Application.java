770c0c2016ed36a261bba89bd700b177e2c93d16
bowook <mbosx316@naver.com>
2024-11-20 18:20:37 +0900

    refacor : 의존성 추가

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,12 +1,14 @@
 package vendingmachine;
 
 import vendingmachine.controller.MachineController;
+import vendingmachine.service.ItemService;
 import vendingmachine.view.InputView;
 import vendingmachine.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        MachineController machineController = new MachineController(new InputView(), new OutputView());
+        MachineController machineController = new MachineController(new InputView(), new OutputView(),
+                new ItemService());
         machineController.start();
     }
 }
