e4f4754b39e2bae1765f62636d0e5f2d525a77eb
bowook <mbosx316@naver.com>
2024-11-20 15:23:47 +0900

    feat(OutputView) : OuputView 기능 추가

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,2 +1,23 @@
-package vendingmachine.view;public class OutputView {
+package vendingmachine.view;
+
+
+import vendingmachine.domain.Machine;
+
+public class OutputView {
+    private final static String HOLD_MACHINE_MONEY = "자판기가 보유한 동전";
+
+    public void writeHoldMachineMoney(Machine machine) {
+        newLine();
+        System.out.println(HOLD_MACHINE_MONEY);
+        System.out.println(machine.initStatus());
+    }
+
+    public void writeErrorMessage(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    private void newLine() {
+        System.out.print(System.lineSeparator());
+    }
+
 }
