81bc1ac4643b77c77a0f6775232d458f224e9507
bowook <mbosx316@naver.com>
2024-11-20 18:21:35 +0900

    feat(Machine) : Domain Machine 기능 추가

--- src/main/java/vendingmachine/domain/Machine.java
+++ src/main/java/vendingmachine/domain/Machine.java
@@ -1,13 +1,171 @@
 package vendingmachine.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import vendingmachine.exception.CustomException;
+import vendingmachine.exception.ErrorMessage;
+
 public class Machine {
+    private final List<Item> items;
     private int fiveHundred = 0;
     private int hundred = 0;
     private int fifty = 0;
     private int ten = 0;
+    private int returnFiveHundred = 0;
+    private int returnHundred = 0;
+    private int returnFifty = 0;
+    private int returnTen = 0;
+
 
     public Machine(final Money initMoney) {
         classification(initMoney);
+        items = new ArrayList<>();
+    }
+
+    public void addItems(List<String> itemStatus) {
+        String itemName = itemStatus.get(0);
+        Money itemPrice = new Money(itemStatus.get(1));
+        String itemQuantity = itemStatus.get(2);
+        items.add(new Item(itemName, itemPrice, itemQuantity));
+    }
+
+    public boolean isPurchasePossible(Money userMoney) {
+        for (Item item : items) {
+            if (userMoney.getMoney() >= item.getMoney()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isPurchaseQuantity() {
+        int sum = 0;
+        for (Item item : items) {
+            sum += item.getQuantity();
+        }
+        return sum != 0;
+    }
+
+    public void subtractRemainCoin(Money userMoney) {
+        if (totalRemainCoins() <= userMoney.getMoney()) {
+            allRemains();
+            return;
+        }
+        checkRemains(userMoney);
+    }
+
+    private void checkRemains(Money userMoney) {
+        while (userMoney.getMoney() != 0) {
+            checkRemainFiveHundred(userMoney);
+            checkRemainHundred(userMoney);
+            checkRemainFifty(userMoney);
+            checkRemainTen(userMoney);
+        }
+    }
+
+    private void checkRemainTen(Money userMoney) {
+        if (userMoney.getMoney() / Coin.COIN_10.getAmount() != 0 && ten > 0) {
+            ten -= 1;
+            returnTen += 1;
+            userMoney.subtract(Coin.COIN_10.getAmount());
+        }
+    }
+
+    private void checkRemainFifty(Money userMoney) {
+        if (userMoney.getMoney() / Coin.COIN_50.getAmount() != 0 && fifty > 0) {
+            fifty -= 1;
+            returnFifty += 1;
+            userMoney.subtract(Coin.COIN_50.getAmount());
+        }
+    }
+
+    private void checkRemainHundred(Money userMoney) {
+        if (userMoney.getMoney() / Coin.COIN_100.getAmount() != 0 && hundred > 0) {
+            hundred -= 1;
+            returnHundred += 1;
+            userMoney.subtract(Coin.COIN_100.getAmount());
+        }
+    }
+
+    private void checkRemainFiveHundred(Money userMoney) {
+        if (userMoney.getMoney() / Coin.COIN_500.getAmount() != 0 && fiveHundred > 0) {
+            fiveHundred -= 1;
+            returnFiveHundred += 1;
+            userMoney.subtract(Coin.COIN_500.getAmount());
+        }
+    }
+
+    private void allRemains() {
+        if (fiveHundred != 0) {
+            returnFiveHundred += fiveHundred;
+        }
+        if (hundred != 0) {
+            returnHundred += hundred;
+        }
+        if (fifty != 0) {
+            returnFifty += fifty;
+        }
+        if (ten != 0) {
+            returnTen += ten;
+        }
+    }
+
+    private int totalRemainCoins() {
+        return (Coin.COIN_500.getAmount() * fiveHundred) + (Coin.COIN_100.getAmount() * hundred)
+                + (Coin.COIN_50.getAmount() * fifty) + (Coin.COIN_10.getAmount() * ten);
+    }
+
+    public void sellItem(Money userMoney, String itemName) {
+        validateItemName(itemName);
+        for (Item item : items) {
+            if (userMoney.getMoney() >= item.getMoney()) {
+                item.subtract();
+                userMoney.subtract(item.getMoney());
+            }
+        }
+    }
+
+    private void validateItemName(String itemName) {
+        for (Item item : items) {
+            if (item.getName().equals(itemName)) {
+                return;
+            }
+        }
+        throw CustomException.from(ErrorMessage.ITEM_NAME_NOT_IN_MACHINE);
+    }
+
+    @Override
+    public String toString() {
+        return remainFiveHundred() + remainHundred() + remainFifty() + remainTen();
+    }
+
+    private String remainTen() {
+        if (returnTen != 0) {
+            return String.format("10원 - %d개\n", returnTen);
+        }
+        return "";
+    }
+
+    private String remainFifty() {
+        if (returnFifty != 0) {
+            return String.format("50원 - %d개\n", returnFifty);
+        }
+        return "";
+    }
+
+    private String remainHundred() {
+        if (returnHundred != 0) {
+            return String.format("100원 - %d개\n", returnHundred);
+        }
+        return "";
+    }
+
+    private String remainFiveHundred() {
+        if (returnFiveHundred != 0) {
+            return String.format("500원 - %d개\n", returnFiveHundred);
+        }
+        return "";
     }
 
     public String initStatus() {
@@ -18,43 +176,62 @@ public class Machine {
     }
 
     private void classification(Money initMoney) {
-        int money = checkFiveHundred(initMoney.getMoney());
-        money = checkHundred(money);
-        money = checkFifty(money);
-        checkTen(money);
-    }
-
-    private void checkTen(int money) {
-        int checkTen = money / Coin.COIN_10.getAmount();
-        if (money >= Coin.COIN_10.getAmount() && checkTen != 0) {
-            this.ten = checkTen;
+        while (initMoney.getMoney() != 0) {
+            List<Integer> coins = initCoins(initMoney);
+            init(initMoney, coins);
         }
     }
 
-    private int checkFifty(int money) {
-        int checkFifty = money / Coin.COIN_50.getAmount();
-        if (money >= Coin.COIN_50.getAmount() && checkFifty != 0) {
-            this.fifty = checkFifty;
-            money -= hundred * Coin.COIN_50.getAmount();
+    private void init(Money initMoney, List<Integer> coins) {
+        int coin = Randoms.pickNumberInList(coins);
+        if (coin == Coin.COIN_500.getAmount()) {
+            initFiveHundred(initMoney);
         }
-        return money;
+        if (coin == Coin.COIN_100.getAmount()) {
+            initHundred(initMoney);
+        }
+        if (coin == Coin.COIN_50.getAmount()) {
+            initFifty(initMoney);
+        }
+        if (coin == Coin.COIN_10.getAmount()) {
+            initTen(initMoney);
+        }
     }
 
-    private int checkHundred(int money) {
-        int checkHundred = money / Coin.COIN_100.getAmount();
-        if (money >= Coin.COIN_100.getAmount() && checkHundred != 0) {
-            this.hundred = checkHundred;
-            money -= hundred * Coin.COIN_100.getAmount();
-        }
-        return money;
+    private void initTen(Money initMoney) {
+        ten += 1;
+        initMoney.subtract(Coin.COIN_10.getAmount());
     }
 
-    private int checkFiveHundred(int money) {
-        int checkFiveHundred = money / Coin.COIN_500.getAmount();
-        if (money >= Coin.COIN_500.getAmount() && checkFiveHundred != 0) {
-            this.fiveHundred = checkFiveHundred;
-            money -= fiveHundred * Coin.COIN_500.getAmount();
+    private void initFifty(Money initMoney) {
+        fifty += 1;
+        initMoney.subtract(Coin.COIN_50.getAmount());
+    }
+
+    private void initHundred(Money initMoney) {
+        hundred += 1;
+        initMoney.subtract(Coin.COIN_100.getAmount());
+    }
+
+    private void initFiveHundred(Money initMoney) {
+        fiveHundred += 1;
+        initMoney.subtract(Coin.COIN_500.getAmount());
+    }
+
+    private List<Integer> initCoins(Money initMoney) {
+        List<Integer> coins = new ArrayList<>();
+        if (initMoney.getMoney() >= Coin.COIN_500.getAmount()) {
+            coins.add(Coin.COIN_500.getAmount());
         }
-        return money;
+        if (initMoney.getMoney() >= Coin.COIN_100.getAmount()) {
+            coins.add(Coin.COIN_100.getAmount());
+        }
+        if (initMoney.getMoney() >= Coin.COIN_50.getAmount()) {
+            coins.add(Coin.COIN_50.getAmount());
+        }
+        if (initMoney.getMoney() >= Coin.COIN_10.getAmount()) {
+            coins.add(Coin.COIN_10.getAmount());
+        }
+        return coins;
     }
 }
