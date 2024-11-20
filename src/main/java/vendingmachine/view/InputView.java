d096391e8a35edbe42e138aafaba1f85122e349d
bowook <mbosx316@naver.com>
2024-11-20 15:23:35 +0900

    feat(InputView) : InputView 기능 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,2 +1,21 @@
-package vendingmachine.view;public class InputView {
+package vendingmachine.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.domain.Money;
+
+public class InputView {
+    private final static String HOLD_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+
+    public Money readHoldMoney() {
+        System.out.println(HOLD_MONEY);
+        return new Money(consoleRead());
+    }
+
+    private String consoleRead() {
+        return Console.readLine();
+    }
+
+    public void consoleClose() {
+        Console.close();
+    }
 }
