3386df8beb4637c252b603e928b622d0b219f78f
bowook <mbosx316@naver.com>
2024-11-20 15:23:16 +0900

    feat(Exceptino) : CustomExcepion Message 추가

--- src/main/java/vendingmachine/exception/ErrorMessage.java
+++ src/main/java/vendingmachine/exception/ErrorMessage.java
@@ -1,2 +1,15 @@
-package vendingmachine.exception;public enum ErrorMessage {
+package vendingmachine.exception;
+
+public enum ErrorMessage {
+    NUMBER_INPUT_ERROR("금액은 숫자여야 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
 }
