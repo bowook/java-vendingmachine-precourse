96d8348b515884b4983369999abde11abcdc55ad
bowook <mbosx316@naver.com>
2024-11-20 15:22:33 +0900

    feat(Money) : Money 도메인 추가

--- src/main/java/vendingmachine/domain/Money.java
+++ src/main/java/vendingmachine/domain/Money.java
@@ -1,2 +1,24 @@
-package vendingmachine.domain;public class Money {
+package vendingmachine.domain;
+
+import vendingmachine.exception.CustomException;
+import vendingmachine.exception.ErrorMessage;
+
+public class Money {
+    private final int money;
+
+    public Money(final String money) {
+        this.money = validate(money);
+    }
+
+    private int validate(String money) {
+        try {
+            return Integer.parseInt(money);
+        } catch (NumberFormatException numberFormatException) {
+            throw CustomException.from(ErrorMessage.NUMBER_INPUT_ERROR);
+        }
+    }
+
+    public int getMoney() {
+        return money;
+    }
 }
