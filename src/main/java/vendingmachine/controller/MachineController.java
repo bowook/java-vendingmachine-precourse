b6e0f7dd6371773984d253c305f44f5d2306de7d
bowook <mbosx316@naver.com>
2024-11-20 15:21:32 +0900

    feat(Controller) : 컨트롤러 생성

--- src/main/java/vendingmachine/controller/MachineController.java
+++ src/main/java/vendingmachine/controller/MachineController.java
@@ -1,2 +1,33 @@
-package vendingmachine.controller;public class MachineController {
+package vendingmachine.controller;
+
+import vendingmachine.domain.Machine;
+import vendingmachine.exception.CustomException;
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
+public class MachineController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public MachineController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        Machine machine = holdMachineMoney();
+        outputView.writeHoldMachineMoney(machine);
+    }
+    
+
+    private Machine holdMachineMoney() {
+        while (true) {
+            try {
+                return new Machine(inputView.readHoldMoney());
+            } catch (CustomException customException) {
+                outputView.writeErrorMessage(customException.getMessage());
+            }
+        }
+    }
 }
