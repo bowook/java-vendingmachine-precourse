2c57507eb78e0259386315a0a84443032e3b2648
bowook <mbosx316@naver.com>
2024-11-20 18:20:58 +0900

    feat(Controller) : 컨트롤러 기능 추가

--- src/main/java/vendingmachine/controller/MachineController.java
+++ src/main/java/vendingmachine/controller/MachineController.java
@@ -1,7 +1,9 @@
 package vendingmachine.controller;
 
 import vendingmachine.domain.Machine;
+import vendingmachine.domain.Money;
 import vendingmachine.exception.CustomException;
+import vendingmachine.service.ItemService;
 import vendingmachine.view.InputView;
 import vendingmachine.view.OutputView;
 
@@ -9,17 +11,70 @@ public class MachineController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final ItemService itemService;
 
-    public MachineController(final InputView inputView, final OutputView outputView) {
+    public MachineController(final InputView inputView, final OutputView outputView, final ItemService itemService) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.itemService = itemService;
     }
 
     public void start() {
         Machine machine = holdMachineMoney();
         outputView.writeHoldMachineMoney(machine);
+        holdMachineItems(machine);
+        Money userMoney = userInputMoney();
+        while (true) {
+            if (!retry(userMoney, machine)) {
+                break;
+            }
+        }
+        outputView.writeRemainMachineMoney(machine);
+        inputView.consoleClose();
     }
-    
+
+    private boolean retry(Money userMoney, Machine machine) {
+        outputView.writeRepeatInputMoney(userMoney);
+        if (!machine.isPurchasePossible(userMoney) || !machine.isPurchaseQuantity()) {
+            machine.subtractRemainCoin(userMoney);
+            return false;
+        }
+        userInputItem(machine, userMoney);
+        return true;
+    }
+
+    private void userInputItem(Machine machine, Money userMoney) {
+        while (true) {
+            try {
+                String itemName = inputView.readInputItem();
+                machine.sellItem(userMoney, itemName);
+                break;
+            } catch (CustomException customException) {
+                outputView.writeErrorMessage(customException.getMessage());
+            }
+        }
+    }
+
+    private Money userInputMoney() {
+        while (true) {
+            try {
+                return inputView.readInputMoney();
+            } catch (CustomException customException) {
+                outputView.writeErrorMessage(customException.getMessage());
+            }
+        }
+    }
+
+    private void holdMachineItems(Machine machine) {
+        while (true) {
+            try {
+                itemService.itemProcess(inputView.readHoldItems(), machine);
+                break;
+            } catch (CustomException customException) {
+                outputView.writeErrorMessage(customException.getMessage());
+            }
+        }
+    }
 
     private Machine holdMachineMoney() {
         while (true) {
